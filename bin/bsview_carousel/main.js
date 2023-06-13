209ebc50f208dc31b3e24f6abc5cbcbd0499c29f
shotatanaka <shota9988@gmail.com>
2023-06-13 13:52:33 +0900

    change url for full-search.cgi

--- bin/bsview_carousel/main.js
+++ bin/bsview_carousel/main.js
@@ -40,7 +40,7 @@ function fullSearch(word) {
     document.body.style.cursor = "default";
   }
 
-  var url = "/full_search.cgi?word=" + encodeURIComponent(word);
+  var url = "/bsview_carousel/full_search.cgi?word=" + encodeURIComponent(word);
   httpReq.open("GET", url, true);
   httpReq.send(null);
   document.body.style.cursor = "wait"
